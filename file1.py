7cdd628b55b6c2b2509093f766ac0cd190f740a1
Emily <emilycweeks1@gmail.com>
2026-02-28 18:52:40 +0000

    2

--- file1.py
+++ file1.py
@@ -4,6 +4,18 @@ import time
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+cam = cv2.VideoCapture(0)
+
+while cam.isOpened():
+    ret, frame = cam.read()
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    cv2.imshow("Camera", frame)
+    cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
@@ -12,13 +24,12 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 model_path = r"c:\Users\emily\AppData\Local\python\pythoncore-3.14-64\hand_landmarker.task"
 
-cam = cv2.VideoCapture(1)
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
 
-while cam.isOpened():
-    ret, frame = cam.read()
-    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    cv2.imshow("Camera", frame)
-
+    options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
 print("hi")
